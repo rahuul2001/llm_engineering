673938d8afd1b9384a4bc7e9ccfa46d5a2caa4d5
Rahul Tangsali <rahuul2001@gmail.com>
2026-01-17 06:14:01 -0800

    completed week 5 day 4

--- week5/implementation/ingest.py
+++ week5/implementation/ingest.py
@@ -2,7 +2,7 @@ import os
 import glob
 from pathlib import Path
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter, MarkdownTextSplitter
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai import OpenAIEmbeddings
